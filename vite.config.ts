74bf1aa5687c7a008c3f5f2034b3f6aab504bf59
Rafael <rojoaquim@gmail.com>
2026-06-22 12:23:28 -0300

    fix: change production base to exact repository case '/Cloudsmith/' to resolve trailing slash 404s

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.NODE_ENV === 'production' ? './' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/Cloudsmith/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
